8d0385efd162758616fa554654612a977ea3bda6
sacharypens <sacharypens@gmail.com>
2018-04-24 22:37:55 +0200

    toevoegen basis view en navigatie

---
+++ BandChecker/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BandChecker.ViewModel
+{
+    class MainWindowViewModel : BaseViewModel
+    {
+        private string frameSource;
+        public string FrameSource
+        {
+            get { return frameSource; }
+            set { frameSource = value; NotifyPropertyChanged(); }
+        }
+
+        public MainWindowViewModel()
+        {
+            FrameSource = "BandPage.xaml";
+            
+        }
+    }
+
+
+}
