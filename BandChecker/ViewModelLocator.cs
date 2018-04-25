7bd57aed8eac8add67508136b996a974d476e2f5
sacharypens <sacharypens@gmail.com>
2018-04-25 16:55:33 +0200

    een bug gefixt in bandView

---
+++ BandChecker/ViewModelLocator.cs
@@ -0,0 +1,31 @@
+﻿using BandChecker.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BandChecker
+{
+    class ViewModelLocator
+    {
+        private static MainWindowViewModel mainWindowViewModel = new MainWindowViewModel();
+        private static BandViewModel bandViewModel = new BandViewModel();
+
+        public static MainWindowViewModel MainWindowViewModel
+        {
+            get
+            {
+                return mainWindowViewModel;
+            }
+        }
+
+        public static BandViewModel BandViewModel
+        {
+            get
+            {
+                return bandViewModel;
+            }
+        }
+    }
+}
